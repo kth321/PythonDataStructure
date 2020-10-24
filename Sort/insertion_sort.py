6213fd446dd108b92b28b8f18024f7de34d97e82
kth.mac <rnxogud136@gmail.com>
2020-10-24 16:19:28 +0900

    insertion sort

---
+++ Sort/insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(seq):
+    length = len(seq)
+    for i in range(1, length):
+        j = i
+        while j > 0 and seq[j-1] > seq[j]:
+            seq[j-1], seq[j] = seq[j], seq[j-1]
+            j -= 1
+    return seq
+
+if __name__ == '__main__':
+    seq = [4, 5, 2, 1, 6, 2, 7, 10, 13, 8]
+    print(bubble_sort(seq))    
